2501fd9809f0fcda8016ac34bb789e437eaf7dc8
Monique Rio <nique.rio@gmail.com>
2015-08-10 23:04:47 -0400

    selects radio button for question; added bootstrap

--- app/assets/javascripts/views/question.js
+++ app/assets/javascripts/views/question.js
@@ -1,11 +1,25 @@
 Kodaly.Views.Question = Backbone.View.extend({
     el: '#question',
+    events: {
+        "click button" : "submit",
+    },
     initialize: function(){
         //Turn choices into collection of choices
         this.choices = new Kodaly.Collections.Choices
         for( var key in this.model.get('choices')){
             this.choices.add({'name': key, 'midi_blob': this.model.attributes.choices[key]});
             
+        }
+    },
+    submit: function(event){
+        event && event.preventDefault(); 
+        var selected = $('input[name=choice]:checked', '#choice-list').val()
+        if(!selected){
+            var alert = $('<div>').addClass("alert alert-danger").attr('role',"alert").html('Select Something!');
+            this.$el.prepend(alert)
+        }else{
+            var alert = $('<div>').addClass("alert alert-success").attr('role',"alert").html("You've selected " + selected);
+            this.$el.prepend(alert)
         }
     },
     render: function() {
@@ -15,7 +29,7 @@ Kodaly.Views.Question = Backbone.View.extend({
     },
     renderOneChoice: function(choice){
         var view = new Kodaly.Views.Choice({ model: choice});
-        $('#choice-list').append(view.render().el);
+        $('button').before(view.render().el);
     },
     renderChoices: function(){
        this.choices.each(this.renderOneChoice, this); 
